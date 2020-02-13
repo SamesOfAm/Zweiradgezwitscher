24628c66d0403d7aa031055fa6439e1946d523a2
Florian Tepelmann <ft@klapproth-koch.de>
2020-02-13 11:32:18 +0100

    Initial commit

---
+++ templates/footer.php
@@ -0,0 +1,19 @@
+<div id="footer-wrapper">
+    <div class="footer-claim">
+        <p>Eine gemeinsame Veranstaltung vom MSC Schleizer Dreieck, der Roth Werkzeugbau GmbH, IFA Freunden Staitz und jungen Mopedfreunden der Umgebung.</p>
+    </div>
+    <div class="footer-address">
+        <p>Auf dem Gelände der ROTH Werkzeugbau GmbH</p>
+        <p>Wöhlsdorf 39</p>
+        <p>07955 Auma-Weidtal</p>
+        <p>50°42&prime;32.0&Prime;N 11°57&prime;35.5&Prime;E</p>
+    </div>
+    <div class="partner-logo-group">
+        <a href="#"><img class="partner-logo" src="../files/images/logos/schleiz-logo.png"></a>
+        <a href="#"><img class="partner-logo" src="../files/images/logos/roth-logo.png"></a>
+    </div>
+    <div class="footer-urls">
+        <p><a href="http://www.msc-schleizer-dreieck.de">www.msc-schleizer-dreieck.de</a></p>
+        <p><a href="http://www.roth-werkzeugbau.de">www.roth-werkzeugbau.de</a></p>
+    </div>
+</div>
---
+++ templates/logos.php
@@ -0,0 +1,14 @@
+<div id="sponsoren">
+    <div class="partner-wrapper">
+        <div class="partner-logo-headline">
+            <h2>Mit freundlicher Unterstützung von:</h2>
+        </div>
+        <div class="partner-logo-group">
+            <a href="#"><img class="partner-logo" src="../files/images/logos/wgv-logo.png"></a>
+            <a href="#"><img class="partner-logo" src="../files/images/logos/sternquell-logo.png"></a>
+            <a href="#"><img class="partner-logo" src="../files/images/logos/sparkasse-logo.png"></a>
+            <a href="#"><img class="partner-logo" src="../files/images/logos/bek-logo.png"></a>
+        </div>
+    </div>
+</div>
+<div id="anfahrt"></div>
